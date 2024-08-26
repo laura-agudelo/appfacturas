668d2f4d83efd2df2dd96648c0d5845eda49454b
laura-agudelo <lauramarianaa189@gmail.com>
2024-08-26 08:10:38 -0500

    clase itemFactura

---
+++ src/com/nombreCompania/appfacturas/model/itemFactura.java
@@ -0,0 +1,39 @@
+package com.nombreCompania.appfacturas.model;
+
+public class itemFactura {
+    private producto producto;
+    private int cantidad;
+
+    // Constructor
+    public itemFactura(producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    // Getters y Setters
+    public producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    // Método para calcular el importe del ítem
+    public float calcularImporte() {
+        return (float) (producto.getPrecio() * cantidad);
+    }
+
+    @Override
+    public String toString() {
+        return producto + "\t" + cantidad + "\t" + calcularImporte();
+    }
+}
